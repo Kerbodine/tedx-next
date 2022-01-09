c8e9021df687b4972efc8ff613342f8bbe628a1e
Michael Tong <michaelyuyangtong@gmail.com>
2022-01-09 11:33:13 +0800

    Fixed speaker page button positioning

--- pages/speakers.js
+++ pages/speakers.js
@@ -6,7 +6,7 @@ export default function Speakers() {
   return (
     <div className="px-8 pt-8 mx-auto max-w-4xl">
       <div className="w-full bg-gray-900 rounded-xl text-white p-8 flex gap-4">
-        <div className="relative">
+        <div>
           <h2 className="text-2xl font-medium leading-6 mb-2">
             Become a TEDxVSA speaker
           </h2>
@@ -15,7 +15,7 @@ export default function Speakers() {
             possimus fugit accusantium molestiae illum molestias quae, tempore,
             officiis, cumque repellat quia necessitatibus aliquam.
           </p>
-          <button className="absolute bottom-0 px-4 py-2 rounded-md bg-white text-black hover:bg-ted-red hover:text-white">
+          <button className="mt-4 px-4 py-2 rounded-md bg-white text-black hover:bg-ted-red hover:text-white">
             Sign up today →
           </button>
         </div>
