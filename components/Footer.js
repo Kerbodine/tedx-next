7092980ae3be5372fd1a1bfac389d82111fb9afa
Michael Tong <michaelyuyangtong@gmail.com>
2022-01-09 10:18:45 +0800

    Added website footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,13 @@
-import React from "react";
+import Logo from "../public/logo-dark.svg";
 
 export default function Footer() {
-  return <div></div>;
+  return (
+    <div className="w-full h-56 bg-black">
+      <div className="mx-auto max-w-4xl text-center px-8 pt-8 text-gray-500">
+        <Logo className="w-72 md:w-84 mx-auto mb-4" />
+        <p>This Independent TEDx event is operated under license from TED.</p>
+        <p>© {new Date().getFullYear()} TEDxVSA, All rights reserved.</p>
+      </div>
+    </div>
+  );
 }
